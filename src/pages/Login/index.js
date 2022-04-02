89ce78a7f4ed0584d66bbc70335a1694f657385f
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-04-02 17:31:38 -0300

    connection of login with the api

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import logoImage from '../../assets/logo.svg'
 import padlock from '../../assets/padlock.png'
+import  api from '../../services/api';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login(){
+    const [userName,setUserName] = useState('');
+    const [password,setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function login(e){
+        e.preventDefault();//evita refresh na página, mantendo o SPA
+        const data = {
+            userName,
+            password,
+        };
+        try{
+            const response = await api.post('api/auth/v1/signin',data);
+            localStorage.setItem('userName',userName);
+            localStorage.setItem('accessToken',response.data.accessToken);
+            localStorage.setItem('accessToken',response.data.refreshToken);
+
+            navigate('books')
+        }catch(error){
+            alert('Login failed');
+        }
+    }
+
     return(
             <div className='login-container'>
                 <section className='form'>
                 <img src={logoImage} alt="Erudio Logo"/>
-                <form>
+                <form onSubmit={login}>
                     <h1>Access your account</h1>
-                    <input placeholder='Username' />
-                    <input type='password' placeholder='Password'/>
+                    <input placeholder='Username' value={userName} onChange={e=>setUserName(e.target.value)} />
+                    <input 
+                    type='password' 
+                    placeholder='Password'
+                    value={password} 
+                    onChange={e=>setPassword(e.target.value)}/>
                     <button className='button' type='submit'>Login</button>
                 </form>
                 </section>
