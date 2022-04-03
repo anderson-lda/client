8eb3aa83c6579af626fafb4b7dffa611ce53db3a
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-04-02 21:02:57 -0300

    added delete and logout

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -13,7 +13,7 @@ export default function Books(){
     const navigate = useNavigate();
 
     useEffect(()=>{
-        api.get('api/Book/v1/asc/5/1',{
+        api.get('api/Book/v1/asc/10/1',{
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
@@ -22,13 +22,40 @@ export default function Books(){
         });
     },[accessToken]);//accessToken é o monitorado, entao sempre que ele mudar outro get é feito
 
+    async function deleteBook(id){
+        try{
+            await api.delete(`api/Book/v1/${id}`,{
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            setBooks(books.filter(book=>book.id !== id));
+        }catch(err){
+            alert('Delete failed');
+        }
+    }
+
+    async function logout(){
+        try{
+            await api.get('api/auth/v1/revoke',{
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            localStorage.clear();
+            navigate('/');
+        }catch(err){
+            alert('Logout failed');
+        }
+    }
+
     return(
         <div className="book-container">
             <header>
                 <img src={logoImage} alt="Erudio"/>
                 <span>Welcome, <strong>{userName.toUpperCase()}</strong>!</span>
                 <Link className="button" to="/book/new">Add new book</Link>
-                <button type="button">
+                <button onClick={logout} type="button">
                     <FiPower size={18} color="#251FC5"/>
                 </button>
             </header>
@@ -47,7 +74,7 @@ export default function Books(){
                         <button type="button">
                             <FiEdit size={20} color="#251FC5"/>
                         </button>
-                        <button type="button">
+                        <button onClick={()=>deleteBook(book.id)} type="button">
                             <FiTrash2 size={20} color="#251FC5"/>
                         </button>
                     </li>
